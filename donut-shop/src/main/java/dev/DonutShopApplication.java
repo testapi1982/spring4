44c236680a4d76a41b15697d1330bfa8d8c23c93
testapi1982 <testapi1982@gmail.com>
2026-01-27 15:04:42 +0200

    adaugare proiect donut-shop

---
+++ donut-shop/src/main/java/dev/DonutShopApplication.java
@@ -0,0 +1,13 @@
+package dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonutShopApplication.class, args);
+	}
+
+}
